efcb23b2b0f5bfae92c235f1a90534aedb075bd1
pikoUsername <galymzhan.amantaj@gmail.com>
2023-12-26 11:35:47 +0600

    Add access denied handler

--- app/base/api/app.py
+++ app/base/api/app.py
@@ -3,6 +3,7 @@ from typing import Tuple
 from fastapi import FastAPI, APIRouter
 
 from app.base.api.config import APIConfig
+from app.base.api.exception import load_basic_exceptions_handlers
 from app.base.api.middlewares.main import load_middlewares
 
 
@@ -11,6 +12,7 @@ def create_app(config: APIConfig, debug: bool = True, lifespan: callable = None)
 
     app = FastAPI(**config.fastapi_kwargs, debug=debug, lifespan=lifespan)
 
+    load_basic_exceptions_handlers(app)
     load_middlewares(app, debug)
 
     return app, router
---
+++ app/base/api/exception.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import TypeVar, Generic
+
+from fastapi import FastAPI, status, Request
+from fastapi.responses import ORJSONResponse
+from pydantic.v1.generics import GenericModel
+from starlette.responses import HTMLResponse, RedirectResponse
+
+from app.auth.exceptions import AccessDenied
+from app.base.logging.logger import get_logger
+
+
+logger = get_logger(__name__)
+
+
+TData = TypeVar("TData")
+
+
+@dataclass(frozen=True)
+class ErrorResult(GenericModel, Generic[TData]):
+    message: str
+    data: TData
+
+
+async def handle_403_access_denied(request: Request, err: AccessDenied):
+    response = RedirectResponse("/", status_code=403)
+    return response
+
+
+async def unknown_exception_handler(request: Request, err: Exception) -> ORJSONResponse:
+    logger.error("Handle error", exc_info=err, extra={"error": err})
+    logger.exception("Unknown error occurred", exc_info=err, extra={"error": err})
+    return ORJSONResponse(
+        ErrorResult(message="Unknown server error has occurred", data=err),
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
+
+
+def load_basic_exceptions_handlers(app: FastAPI):
+    app.add_exception_handler(AccessDenied, handle_403_access_denied)
+    app.add_exception_handler(Exception, unknown_exception_handler)
